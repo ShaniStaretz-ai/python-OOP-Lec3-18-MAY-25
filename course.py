77d178864ac96bf6f6765cbc93a619185198ad29
ShaniStaretz <shakimi88@gmail.com>
2025-05-18 19:27:46 +0300

    complete class exercise

--- course.py
+++ course.py
@@ -1,25 +1,29 @@
 from student import Student
+
+
 class Course:
 
-    def __init__(self, _name, _size, _students=None):
-        self.name=_name
-        self.size=_size
+    def __init__(self, _name, _size, _students=None)->None:
+        self.name = _name
+        self.size = _size
         if _students is None:
             _students = []
-        self.student_list=_students
+        self.student_list = _students
 
-    def add_student(self,new_student:Student):
-        if len(self.student_list)<self.size:
+    def add_student(self, new_student: Student) -> None:
+        if len(self.student_list) < self.size:
             self.student_list.append(new_student)
-            print("the student was added successfully")
+            print(f"the student with name '{new_student.name}' was added successfully")
         else:
             print(f"sorry dear {new_student.name}, the course is full, no place for new students")
-    def __str__(self):
+
+    def __str__(self)->str:
         return f"name:{self.name},size:{self.size},students list:{self.student_list}"
 
-    def print_students_list(self):
+    def print_students_list(self)->None:
+        print("the students in the course are:")
         print(self.student_list)
 
-if __name__ == '__main__':
-    c1=Course("shani",9)
 
+if __name__ == '__main__':
+    c1 = Course("shani", 9)
--- student.py
+++ student.py
@@ -6,17 +6,17 @@ class Student:
     #     self.weight = ""
     #     self.age = ""
     #     self.address = ""
-    def __init__(self,_name,_height,_weight,_age,_address):
+    def __init__(self,_name,_height,_weight,_age,_address)->None:
         self.name=_name
         self.height = _height
         self.weight = _weight
         self.age = _age
         self.address = _address
 
-    def __str__(self):
+    def __str__(self)->str:
         return f"{self.name},{self.age},{self.address},{self.address},{self.weight},{self.height}"
 
-    def __repr__(self):
+    def __repr__(self)->str:
         return f"Student({self.name},{self.age},{self.address},{self.address},{self.weight},{self.height})"
 
 
